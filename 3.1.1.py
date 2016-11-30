34f1e0afec1f2550a2eb197f277e2df842bf4749
vuhoang1998 <rabac1998@gmail.com>
2016-11-30 16:34:55 +0700

    Create 3.1.1.py

---
+++ 3.1.1.py
@@ -0,0 +1,13 @@
+# Homework-session-3
+from turtle import*
+color("red")
+def doit(a,b):
+    a(b)
+    forward(100)
+for i in range(4):
+         doit(rt,30)
+         doit(lt,60)
+         doit(lt,120)
+         doit(lt,60)
+         rt(120)
+         
